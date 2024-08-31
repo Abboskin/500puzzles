68e17793e9b6bbd97326ea0bdffad2bfcaa2b6b0
unknown <abboskinsenpai@gmail.com>
2024-08-31 12:23:37 +0500

    Add Puzzle2.sql

---
+++ Puzzle2.sql
@@ -0,0 +1,38 @@
+CREATE TABLE #lag
+(
+BusinessEntityID INT
+,SalesYear   INT
+,CurrentQuota  DECIMAL(20,4)
+)
+GO
+ 
+INSERT INTO #lag
+SELECT 275 , 2005 , '367000.00'
+UNION ALL
+SELECT 275 , 2005 , '556000.00'
+UNION ALL
+SELECT 275 , 2006 , '502000.00'
+UNION ALL
+SELECT 275 , 2006 , '550000.00'
+UNION ALL
+SELECT 275 , 2006 , '1429000.00'
+UNION ALL
+SELECT 275 , 2006 ,  '1324000.00'
+
+--Solution 1
+
+select *, 
+       lead(CurrentQuota, 1, 0) over (order by (select null)) as lagCurrentData
+from #lag;
+
+
+
+--Solution from site 
+;WITH CTE AS
+(
+    SELECT BusinessEntityID,SalesYear,CurrentQuota,ROW_NUMBER() OVER (ORDER BY %%Physloc%%) rnk  
+    FROM lag
+)
+SELECT    BusinessEntityID,SalesYear,CurrentQuota
+          ,ISNULL(LEAD(CurrentQuota) OVER (ORDER BY rnk),0) NextCurrentData     
+FROM CTE c1
